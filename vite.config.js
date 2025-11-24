67925f64f0cf5a1afbae3a86ede474f7cd751a23
Gregory <57194668+Greg50100@users.noreply.github.com>
2025-11-24 17:13:03 +0100

    feat: add GitHub Actions workflow for deploying to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // IMPORTANT : Le nom du dépôt GitHub entre les slashes.
+  // Si votre dépôt s'appelle "mon-portfolio", mettez base: '/mon-portfolio/'
+  base: '/personnal-site/', 
 })
